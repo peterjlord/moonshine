25a6bafd5a922d41989e499b01657223fa647dd7
Peter J Lord <me@peterjlord.co.uk>
2016-11-04 14:29:25 +0000

    Gulp svg workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
+'use strict';
+
 var gulp = require('gulp');
 var compass = require('gulp-compass');
 var shell   = require('gulp-shell');
+var svgSymbols = require('gulp-svg-symbols');
+
 
 gulp.task('compass', function() {
   gulp.src(['./sass/*.scss', './sass/lib/*.scss'])
@@ -12,14 +16,38 @@ gulp.task('compass', function() {
      .on('error',console.log.bind(console)) //Error handling and 
     .pipe(gulp.dest('css'));
 });
+
 gulp.task('clearcache', function() {
   return shell.task([
     'drush cc all'
   ]);
 });
 
+gulp.task('icons', function () {
+	return gulp.src('./images/svg-inuse/*.svg')
+	.pipe(svgSymbols({
+		svgClassname: 'svg-icon-lib',
+		title: '%f Icon',
+		slug: function (name) {
+    	return name.replace(/-/g, '').replace(/[0-9]/g, '');
+		},
+		transformData: function(svg, defaultData, options) {
+		return {
+    // Return every datas you need 
+    id:         defaultData.id,
+    className:  defaultData.className,
+    width:      32 + 'px',
+    height:     32 + 'px'
+  
+		};
+		}
+	}))
+  .pipe(gulp.dest('./images/'));
+
+});
+
 gulp.task('watch', function () {
-  gulp.watch(['sass/*.scss', 'sass/**/*.scss'], ['compass']);
+  gulp.watch(['sass/*.scss', 'sass/**/*.scss', 'images/svg-inuse/*.svg'], ['compass', 'icons']);
 });
 
 
