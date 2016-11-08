d1d0126e9cf0ccfa14d346c6a727f12f37489aa6
Peter J Lord <me@peterjlord.co.uk>
2016-11-08 16:55:26 +0000

    Nav search css / js. Need to stop submiting

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@
 	Drupal.behaviors.moonshine= {
 		attach: function (context, settings) {
 			// Click to expand search.
-		/*
     $('#search-block-form').submit(function (e) {
-      var $header = $(this).parents('#nav-header');
+			
+      var $header = $(this).parents('header.centered-navigation');
       if (!$header.hasClass('search-expanded')) {
         $header.addClass('search-expanded');
         this.querySelector('input[type="text"]').focus();
@@ -13,19 +13,13 @@
     }).find('input').blur(function () {
       // Wait for next element to gain focus.
       setTimeout(function () {
-        var trigger = document.querySelector('#nav-header.search-expanded');
+        var trigger = document.querySelector('header.search-expanded');
         // Check if focused element is still in the expanded menu.
         if (trigger !== null && !trigger.querySelector('#search-block-form').contains(document.activeElement)) {
           trigger.classList.remove('search-expanded');
         }
       }, 150);
     });
-    // Skip to search a11y link.
-    $('.skip-link-search').click(function (e) {
-      $('#search-block-form').submit();
-      e.preventDefault();
-    });
-		*/
 		}
  	};
 })(jQuery);
