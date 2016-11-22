be7640ee6794394c4308c6fd80338691c6e2c564
Peter J Lord <me@peterjlord.co.uk>
2016-11-22 16:12:38 +0000

    Updated js to be drupal freindly

--- js/jquery.responsive-classes.js
+++ js/jquery.responsive-classes.js
@@ -1,35 +1,34 @@
-// Change width value on page load
-$(document).ready(function(){
-    responsive_resize();
-});
-
-// Change width value on user resize, after DOM
-$(window).resize(function(){
-     responsive_resize();
-});
-
-  function responsive_resize(){
-	 var current_width = $(window).width();
-    //do something with the width value here!
-	 if(current_width < 481)
+(function ($) {
+  Drupal.behaviors.responsive_classes= {
+    attach: function (context, settings) {
+      $('html', context).once('response_classes').each(function(context) {
+        responsive_classes(context);
+      });
+    }
+  };
+  function responsive_classes(context) {
+		// Put the jquery here
+		$(document).ready(function(){
+    	responsive_set_classes();
+		});
+		// Change width value on user resize, after DOM
+		$(window).resize(function(){
+     responsive_set_classes();
+		});	
+  }
+	function responsive_set_classes() {
+		var current_width = $(window).width();
+		
+		if(current_width < 481)
       $('html').addClass("m320").removeClass("m768").removeClass("desktop").removeClass("m480");
- 
+
     else if(current_width < 739)
       $('html').addClass("m768").removeClass("desktop").removeClass("m320").removeClass("tablet");
- 
+
     else if (current_width < 970)
       $('html').addClass("tablet").removeClass("desktop").removeClass("m320").removeClass("m768");
- 
-    else if (current_width > 971)
+
+    else (current_width > 971)
       $('html').addClass("desktop").removeClass("m320").removeClass("m768").removeClass("tablet");
- 
-    if(current_width < 650){
-      $('html').addClass("mobile-menu").removeClass("desktop-menu");
-	 $('.sf-menu').removeClass("sf-js-enabled").addClass("sf-js-disabled");
 	}
- 
-    if(current_width > 651){
-      $('html').addClass("desktop-menu").removeClass("mobile-menu");
-	  $('.sf-menu').removeClass("sf-js-disabled").addClass("sf-js-enabled");
-	}
-}
+})(jQuery);
